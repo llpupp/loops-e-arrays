524ca77df4125ad24b4d9e0ea9e4db56212b6052
llpupp <llpupp@hotmail.com>
2022-10-28 09:29:36 -0300

    Consoantes

---
+++ src/dio/exercicios/arrays/Ex2_Consoantes.java
@@ -0,0 +1,46 @@
+package dio.exercicios.arrays;
+/*
+Faça um Programa que leia um vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
+
+import java.util.Scanner;
+
+public class Ex2_Consoantes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeConsoantes = 0;
+
+        int count = 0;
+        do {
+            System.out.println("Letra: ");
+            String letra = scan.next();
+
+            if ( !(letra.equalsIgnoreCase("a") |
+                    letra.equalsIgnoreCase("e") |
+                    letra.equalsIgnoreCase("i") |
+                    letra.equalsIgnoreCase("o") |
+                    letra.equalsIgnoreCase("u")) )  {
+                consoantes[count] = letra;
+                quantidadeConsoantes++;
+            }
+
+            count++;
+
+        } while(count < consoantes.length);
+
+        System.out.print("Consoantes: ");
+        for ( String consoante  :  consoantes ) { //quer dizer para cada elemento dentro do array consoantes, imprima o elemento (consoante)
+            if ( consoante != null) //se consoante for diferente de null, imprima consoante
+                System.out.print(consoante + " ");
+        }
+        System.out.println("\nQuantidade de consoantes: " + quantidadeConsoantes);
+        System.out.println("Comprimento do array: " + consoantes.length);
+
+    }
+
+}
+
